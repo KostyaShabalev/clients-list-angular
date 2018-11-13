ef9aeb0e0bdf01c341fdeaa66f87dce0c729e175
Kostya Shabalev <kvshabalev@gmail.com>
2018-11-13 11:37:01 +0200

    first commit - markup

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ClientsMainComponent } from './clients-main/clients-main.component';
+import { ClientsListComponent } from './clients-list/clients-list.component';
+import { InputComponent } from './input/input.component';
+import { ClientDetailsComponent } from './client-details/client-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ClientsMainComponent,
+    ClientsListComponent,
+    InputComponent,
+    ClientDetailsComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
